243aa927bbe7531e10ffa29ab305b053891f91d8
imransarkerweb <imransarker.web@gmail.com>
2023-05-30 14:09:08 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("boss is running");
+});
+app.listen(port, () => {
+  console.log(`Bistro boss is running at port ${port}`);
+});
