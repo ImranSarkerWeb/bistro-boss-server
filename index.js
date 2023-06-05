e3cecf4973286c56b8bc783b3b057ee62eac449b
imransarkerweb <imransarker.web@gmail.com>
2023-06-06 01:14:35 +0600

    api updated

--- index.js
+++ index.js
@@ -29,9 +29,27 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const userCollection = client.db("bossDB").collection("users");
     const menuCollection = client.db("bossDB").collection("menu");
     const reviewCollection = client.db("bossDB").collection("reviews");
     const cartCollection = client.db("bossDB").collection("carts");
+
+    //user api
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "User Alyready Exist" });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
 
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
@@ -53,8 +71,19 @@ async function run() {
     });
     app.post("/carts", async (req, res) => {
       const item = req.body;
-      console.log(item);
+
       const result = await cartCollection.insertOne(item);
+      res.send(result);
+    });
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
       res.send(result);
     });
 
